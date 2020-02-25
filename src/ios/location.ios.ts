0a309aa3179992fd0da38cca74c448066d7fdb40
hazalozturk <hazal.ozturk@troyweb.com>
2020-02-25 10:17:25 -0500

    Add startTracking

--- src/ios/location.ios.ts
+++ src/ios/location.ios.ts
@@ -1,7 +1,6 @@
 import { MapboxView, MGLMapViewDelegateImpl } from "../mapbox-sdk.ios";
 import { MapboxLocation, LocationOptions } from "../common/location.common";
 
-
 const _getTrackingMode = (input: LocationOptions["cameraMode"]): MGLUserTrackingMode => {
     if (input === "TRACKING") {
       return MGLUserTrackingMode.Follow;
@@ -12,6 +11,28 @@ const _getTrackingMode = (input: LocationOptions["cameraMode"]): MGLUserTracking
     } else {
       return MGLUserTrackingMode.None;
     }
+};
+
+const _stringToCameraMode = ( mode: LocationOptions["cameraMode"]): any => {
+  switch ( mode ) {
+
+    case "NONE":
+
+      console.log( "NONE" );
+      return 0;
+
+    case "TRACKING_COMPASS":
+
+      console.log( "TRACKING_COMPASS" );
+      return 1;
+
+
+    case "TRACKING_GPS":
+
+      console.log('TRACKING_GPS');
+      return 3;
+
+  }
 };
 
 export class Location extends MapboxLocation {
@@ -29,15 +50,26 @@ export class Location extends MapboxLocation {
   private _getLocationComponentOptions() {}
 
   startTracking(options: LocationOptions): Promise<void> {
+    let that = this;
+
     return new Promise((resolve, reject) => {
+      let camera = that.mapboxView["nativeMapView"].camera;
+      camera.pitch = options.tilt;
+      const durationMs = options.animationDuration ? options.animationDuration : 5000;
+
       try {
         if (!this.mapboxView["nativeMapView"]) {
           reject("No map has been loaded");
           return;
         }
-        this.mapboxView["nativeMapView"].showsUserLocation = true;
-        this.mapboxView["nativeMapView"].setUserTrackingModeAnimated(_getTrackingMode(options.cameraMode), options.animated === true);
-        this.mapboxView["nativeMapView"].userTrackingMode =  1;
+        that.mapboxView["nativeMapView"].showsUserLocation = true;
+        that.mapboxView["nativeMapView"].setUserTrackingModeAnimated(_getTrackingMode(options.cameraMode), options.animated === true);
+        that.mapboxView["nativeMapView"].userTrackingMode =  _stringToCameraMode( options.cameraMode );
+
+        that.mapboxView["nativeMapView"].setCameraWithDurationAnimationTimingFunction(
+          camera,
+          durationMs / 1000,
+          CAMediaTimingFunction.functionWithName(kCAMediaTimingFunctionEaseInEaseOut));
 
         resolve();
       } catch (ex) {
@@ -47,5 +79,9 @@ export class Location extends MapboxLocation {
     });
   }
 
-  stopTracking() {}
+  stopTracking() {
+    this.mapboxView["nativeMapView"].camera.pitch = 0;
+    this.mapboxView["nativeMapView"].camera.heading = 180;
+    this.mapboxView["nativeMapView"].userTrackingMode =  0;
+  }
 }
