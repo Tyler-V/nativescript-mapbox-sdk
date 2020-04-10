e2ec66c2d1e0638a896b98c9c1d111369801038c
Tyler Vorpahl <tyler.vorpahl@gmail.com>
2020-04-10 15:27:13 -0400

    setupSymbolLayer

--- demo-angular/src/app/map/map.component.ts
+++ demo-angular/src/app/map/map.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { ModalDialogService, ModalDialogOptions } from 'nativescript-angular/modal-dialog';
-import { LatLng } from 'nativescript-mapbox-sdk';
+import { LatLng, SymbolLayerOptions, MapboxColor, LayerOptions, Feature } from 'nativescript-mapbox-sdk';
 import { MapService } from './map.service';
 import { StylesComponent } from './styles/styles.component';
 import { OfflineComponent } from './offline/offline.component';
 import { LocationComponent } from './location/location.component';
 import { LayersComponent } from './layers/layers.component';
+import { isAndroid, isIOS } from 'tns-core-modules/platform';
+import { android as androidApp } from 'tns-core-modules/application';
+
+declare const com, java, android;
 
 @Component({
     selector: 'map',
@@ -34,8 +38,11 @@ export class MapComponent implements OnInit {
         this.mapService.mapView = args.object.mapView;
         this.mapService.mapbox.map.addOnMapClickListener((latLng: LatLng) => {
             console.log(latLng);
-            const features = this.mapService.mapbox.map.queryRenderedFeatures(latLng);
+            const features = this.mapService.mapbox.map.queryRenderedFeatures(latLng, 'symbol-layer-id');
             console.log(features);
+            if (features.length > 0) {
+                this.setupSymbolLayer(features[0]);
+            }
         });
         this.mapService.mapbox.map.addOnMapLongClickListener((latLng: LatLng) => {
             console.log(latLng);
@@ -47,6 +54,9 @@ export class MapComponent implements OnInit {
 
     onStyleLoaded(args) {
         console.log(args.eventName);
+        this.mapService.mapbox.style.addVectorSource('wells', 'mapbox://tvorpahl.b31830kk');
+        this.addHeatmapLayer();
+        this.addSymbolLayer();
     }
 
     onCameraMove(args) {
@@ -63,14 +73,7 @@ export class MapComponent implements OnInit {
         return this.modalService.showModal(LocationComponent, options);
     }
 
-    showOfflineModal() {
-        const options: ModalDialogOptions = {
-            viewContainerRef: this.vcRef,
-            fullscreen: true,
-        };
-
-        return this.modalService.showModal(OfflineComponent, options);
-    }
+    showOfflineModal() {}
 
     showStylesModal() {
         const options: ModalDialogOptions = {
@@ -88,5 +91,64 @@ export class MapComponent implements OnInit {
         };
 
         return this.modalService.showModal(LayersComponent, options);
+    }
+
+    private addHeatmapLayer() {
+        const options: LayerOptions = {
+            maxZoom: 13,
+        };
+        this.mapService.heatmapLayer = this.mapService.mapbox.style.layers.heatmap.create('heatmap-layer-id', 'wells', options);
+        this.mapService.mapbox.style.layers.heatmap.setHeatmapColor(this.mapService.heatmapLayer, [
+            [0, new MapboxColor(255, 255, 255, 0.01)],
+            [0.25, new MapboxColor(4, 179, 183)],
+            [0.5, new MapboxColor(204, 211, 61)],
+            [0.75, new MapboxColor(252, 167, 55)],
+            [1.0, new MapboxColor(255, 78, 70)],
+        ]);
+        this.mapService.mapbox.style.layers.heatmap.setHeatmapIntensity(this.mapService.heatmapLayer, [
+            [0, 3],
+            [options.maxZoom, 1],
+        ]);
+        this.mapService.mapbox.style.layers.heatmap.setHeatmapRadius(this.mapService.heatmapLayer, [
+            [0, 3],
+            [10, 8],
+            [options.maxZoom, 25],
+        ]);
+        this.mapService.mapbox.style.layers.heatmap.setHeatmapOpacity(this.mapService.heatmapLayer, [
+            [0, 1],
+            [options.maxZoom, 1],
+        ]);
+        this.mapService.mapbox.style.addLayer(this.mapService.heatmapLayer);
+    }
+
+    private addSymbolLayer() {
+        this.mapService.mapbox.style.addImage('OIL', 'images/types/oil.png');
+        this.mapService.mapbox.style.addImage('GAS', 'images/types/gas.png');
+        this.mapService.mapbox.style.addImage('OILGAS', 'images/types/oilgas.png');
+        this.mapService.mapbox.style.addImage('EOR', 'images/types/eor.png');
+        this.mapService.mapbox.style.addImage('SWD', 'images/types/swd.png');
+        this.mapService.mapbox.style.addImage('OTHER', 'images/types/other.png');
+
+        const options: SymbolLayerOptions = {
+            minZoom: 13,
+            iconImageKey: 'TYPE',
+            iconSize: isAndroid ? 2 : 0.75,
+            iconAllowOverlap: true,
+            iconIgnorePlacement: true,
+        };
+        this.mapService.symbolLayer = this.mapService.mapbox.style.layers.symbolLayer.create('symbol-layer-id', 'wells', options);
+        this.mapService.mapbox.style.addLayer(this.mapService.symbolLayer);
+    }
+
+    setupSymbolLayer(feature: Feature) {
+        console.log(feature);
+        let resourceId = androidApp.context
+            .getResources()
+            .getIdentifier('activity_query_feature_window_symbol_layer', 'layout', androidApp.context.getPackageName());
+        const inflater: typeof com.mapbox.mapboxsdk.annotations.BubbleLayout = android.view.LayoutInflater.from(androidApp.foregroundActivity);
+        const bubbleLayout = inflater.inflate(resourceId, null);
+        const titleTextView = bubbleLayout.findViewById('info_window_title');
+        titleTextView.setText('Test');
+        const stringBuilder = new java.lang.StringBuilder();
     }
 }
