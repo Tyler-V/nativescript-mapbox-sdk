d695d71ae80ca16eb01b7e1e3653c64d598d379c
Tyler Vorpahl <tyler.vorpahl@gmail.com>
2020-06-11 15:14:20 -0400

    ios line optoins

--- src/ios/layers/line.ios.ts
+++ src/ios/layers/line.ios.ts
@@ -1,4 +1,5 @@
 import { MapboxLineLayer, LineLayerOptions } from '../../common/layers/line.common';
+import { mapboxColorToColor } from '../utils.ios';
 
 export class LineLayer extends MapboxLineLayer {
   create(layerId: string, sourceId: string, options?: LineLayerOptions) {
@@ -9,6 +10,8 @@ export class LineLayer extends MapboxLineLayer {
     if (options) {
       if (options.minZoom) layer.minimumZoomLevel = options.minZoom;
       if (options.maxZoom) layer.maximumZoomLevel = options.maxZoom;
+      if (options.lineWidth) layer.lineWidth = NSExpression.expressionForConstantValue(options.lineWidth);
+      if (options.lineColor) layer.lineColor = NSExpression.expressionForConstantValue(mapboxColorToColor(options.lineColor));
     }
 
     return layer;
