7129af7571b3948ab1f6e7765c51a9add4724bb8
Tyler Vorpahl <tyler.vorpahl@gmail.com>
2021-02-20 18:19:47 -0500

    addOnMapPanListener

--- src/ios/map.ios.ts
+++ src/ios/map.ios.ts
@@ -156,15 +156,16 @@ export class Map extends MapboxMap {
 
   addOnMapPanListener(listener: (event: MapPanEvent) => void) {
     this.view.mapView.mapPanHandler = MapPanHandlerImpl.initWithOwnerAndListenerForMap(new WeakRef(this), listener, this.view.mapView);
+    const panGestureRecognizer = UIPanGestureRecognizer.alloc().initWithTargetAction(this.view.mapView.mapLongClickHandler, 'pan');
 
     for (let i = 0; i < this.view.mapView.gestureRecognizers.count; i++) {
       let recognizer: UIGestureRecognizer = this.view.mapView.gestureRecognizers.objectAtIndex(i);
-
       if (recognizer instanceof UIPanGestureRecognizer) {
-        recognizer.addTargetAction(this.view.mapView.mapPanHandler, 'pan');
-        break;
+        panGestureRecognizer.requireGestureRecognizerToFail(recognizer);
       }
     }
+
+    this.view.mapView.addGestureRecognizer(panGestureRecognizer);
   }
 
   getZoom() {
