04db139636d3e8ab4284521659d62f62d8e0dcae
Tyler Vorpahl <tyler.vorpahl@gmail.com>
2021-02-22 19:07:56 -0500

    Add showMap property to enable debugging of events

--- demo-angular/src/app/map/map.component.ts
+++ demo-angular/src/app/map/map.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { ModalDialogService, ModalDialogOptions } from '@nativescript/angular';
-import { isAndroid, isIOS } from '@nativescript/core';
-import { LatLng, SymbolLayerOptions, MapboxColor, LayerOptions } from 'nativescript-mapbox-sdk';
+import { LatLng, MapPanEvent } from 'nativescript-mapbox-sdk';
 import { MapService } from './map.service';
 import { StylesComponent } from './styles/styles.component';
-import { OfflineComponent } from './offline/offline.component';
 import { LocationComponent } from './location/location.component';
 import { LayersComponent } from './layers/layers.component';
 import { AndroidCalloutService } from './callouts/android-callout.service';
@@ -18,6 +16,7 @@ import { IOSCalloutService } from './callouts/ios-callout.service';
     providers: [AndroidCalloutService, IOSCalloutService],
 })
 export class MapComponent implements OnInit {
+    showMap: boolean = false;
     accessToken: string = 'sk.eyJ1IjoidHZvcnBhaGwiLCJhIjoiY2s1dml5YXlxMHNncTNnbXgzNXVnYXQ0NyJ9.y0ofxDzXB4vi6KW372rLEQ';
     mapStyle: string = 'mapbox://styles/mapbox/streets-v11';
     latitude: number = 39.8283;
@@ -45,13 +44,20 @@ export class MapComponent implements OnInit {
         this.mapService.mapboxView = event.object;
         this.mapService.mapbox = event.object.mapbox;
         this.mapService.mapView = event.object.mapView;
-        this.mapService.mapReady.next(true);
-        this.mapService.mapbox.map.addOnMapLongClickListener((latLng: LatLng) => {
-            console.log(latLng);
-            const bounds = this.mapService.mapbox.map.getBounds();
-            const features = this.mapService.mapbox.map.queryRenderedFeaturesByBounds(bounds);
-            console.log(features);
+
+        this.mapService.mapbox.map.addOnMapClickListener((latLng: LatLng) => {
+            console.log(`MapClickListener ${JSON.stringify(latLng)}`);
         });
+
+        this.mapService.mapbox.map.addOnMapLongClickListener((latLng: LatLng) => {
+            console.log(`MapLongClickListener ${JSON.stringify(latLng)}`);
+        });
+
+        this.mapService.mapbox.map.addOnMapPanListener((event: MapPanEvent) => {
+            console.log(`MapPanListener ${event}`);
+        });
+
+        this.mapService.mapReady.next(true);
     }
 
     onStyleLoaded(event) {
